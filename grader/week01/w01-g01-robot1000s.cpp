33c6d4bff5ab8bf08d8a70a7700032c3a9bd885d
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-01-16 01:34:36 +0700

    include map

--- grader/week01/w01-g01-robot1000s.cpp
+++ grader/week01/w01-g01-robot1000s.cpp
@@ -1,49 +1,26 @@
 #include <iostream>
-#include <cstdlib>
 #include <algorithm>
+#include <map>
 using namespace std;
 int main()
 {
-    int count[4] = {0,0,0,0};
-    string s;
-    int k;
+    map<char, int> di;// = {{'N', 0}, {'S', 1}, {'E', 2}, {'W', 3}};
+    int t = 0, k, mx = -1, count[4] = {0,0,0,0};
+    string s = "NSEW";
+    for(int i=0;i<4;i++)
+        di[s[i]] = i;
     cin >> s >> k;
-    int i = 0;
-    while(s[i]!='\0')
+    while(s[t]!='\0')
     {
-        switch(s[i])
-        {
-            case 'N':
-                count[0]+=1;
-                break;
-            case 'S':
-                count[1]+=1;
-                break;
-            case 'E':
-                count[2]+=1;
-                break;
-            case 'W':
-                count[3]+=1;
-                break;
-        }
-        i++;
+        count[di[s[t]]]++;
+        t++;
     }
-    int mx = -1;
     for(int i=0;i<=min(k,count[0]);i++)
-    {
         for(int j=0;j<=min(k,count[1]);j++)
-        {
             for(int u=0;u<=min(k,count[2]);u++)
-            {
                 for(int h=0;h<=min(k,count[3]);h++)
-                {
                     if(i+j+u+h == k)
                         mx = max(mx,abs(count[0]-i-count[1]+j)+abs(count[2]-u-count[3]+h));
-                }
-            }
-        }
-        
-    }
     cout << mx*2 << endl;
     return 0;
 }
