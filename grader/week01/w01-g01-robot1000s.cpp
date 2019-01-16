749df261cd44892ce1e26b26a9751e9cf8e576a6
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-01-16 10:50:39 +0700

    shorter+faster

--- grader/week01/w01-g01-robot1000s.cpp
+++ grader/week01/w01-g01-robot1000s.cpp
@@ -5,20 +5,17 @@ using namespace std;
 int main()
 {
     map<char, int> di;// = {{'N', 0}, {'S', 1}, {'E', 2}, {'W', 3}};
-    int t = 0, k, mx = -1, count[4] = {0,0,0,0};
+    int k, mx = -1, count[4] = {0,0,0,0};
     string s = "NSEW";
     for(int i=0;i<4;i++)
         di[s[i]] = i;
     cin >> s >> k;
-    while(s[t]!='\0')
-    {
+    for(int t=0;s[t]!='\0',t++)
         count[di[s[t]]]++;
-        t++;
-    }
     for(int i=0;i<=min(k,count[0]);i++)
-        for(int j=0;j<=min(k,count[1]);j++)
-            for(int u=0;u<=min(k,count[2]);u++)
-                for(int h=0;h<=min(k,count[3]);h++)
+        for(int j=0;j<=min(k-i,count[1]);j++)
+            for(int u=0;u<=min(k-i-j,count[2]);u++)
+                for(int h=0;h<=min(k-i-j-u,count[3]);h++)
                     if(i+j+u+h == k)
                         mx = max(mx,abs(count[0]-i-count[1]+j)+abs(count[2]-u-count[3]+h));
     cout << mx*2 << endl;
