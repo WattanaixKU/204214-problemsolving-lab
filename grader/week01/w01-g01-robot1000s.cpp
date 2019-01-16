8e5300d634c1382bf81544da92f875580daac27f
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-01-16 11:49:03 +0700

    >= c11 only

--- grader/week01/w01-g01-robot1000s.cpp
+++ grader/week01/w01-g01-robot1000s.cpp
@@ -4,13 +4,11 @@
 using namespace std;
 int main()
 {
-    map<char, int> di;// = {{'N', 0}, {'S', 1}, {'E', 2}, {'W', 3}};
+    map<char, int> di = {{'N', 0}, {'S', 1}, {'E', 2}, {'W', 3}};
     int k, mx = -1, count[4] = {0,0,0,0};
-    string s = "NSEW";
-    for(int i=0;i<4;i++)
-        di[s[i]] = i;
+    string s;
     cin >> s >> k;
-    for(int t=0;s[t]!='\0',t++)
+    for(int t=0;s[t]!='\0';t++)
         count[di[s[t]]]++;
     for(int i=0;i<=min(k,count[0]);i++)
         for(int j=0;j<=min(k-i,count[1]);j++)
