fdd1f2c1fcc8d99179f1669d9ba08c59677b1ae8
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-01-20 16:11:56 +0700

    faster & Graded

--- grader/week01/w01-g01-robot1000s.cpp
+++ grader/week01/w01-g01-robot1000s.cpp
@@ -13,9 +13,8 @@ int main()
     for(int i=0;i<=min(k,count[0]);i++)
         for(int j=0;j<=min(k-i,count[1]);j++)
             for(int u=0;u<=min(k-i-j,count[2]);u++)
-                for(int h=0;h<=min(k-i-j-u,count[3]);h++)
-                    if(i+j+u+h == k)
-                        mx = max(mx,abs(count[0]-i-count[1]+j)+abs(count[2]-u-count[3]+h));
+                if(k-i-j-u <= count[3])
+                    mx = max(mx,abs(count[0]-i-count[1]+j)+abs(count[2]-u-count[3]+(k-i-j-u)));
     cout << mx*2 << endl;
     return 0;
 }
