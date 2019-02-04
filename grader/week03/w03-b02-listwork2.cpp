497d7cae51e1aeee953dabd10ce2bf26f75b4236
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-02-05 00:25:51 +0700

    passed

---
+++ grader/week03/w03-b02-listwork2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <list>
+using namespace std;
+int main()
+{
+    int n,inp;
+    char mode;
+    cin >>n;
+    list<int> ll;
+    for(int i=0;i<n;i++)
+    {
+        cin >> mode >> inp;
+        if(mode == 'I')
+            ll.push_front(inp);
+        else if(mode == 'A')
+            ll.push_back(inp);
+        else
+        {
+            //ll.erase(ll.begin()+inp);
+            list<int>::iterator it = ll.begin();
+            while(inp!=1 && it!=ll.end())
+            {
+                it++;
+                inp--;
+            }
+            if(inp==1 && it!=ll.end())
+                ll.erase(it);
+        }
+    }
+    for(list<int>::iterator it = ll.begin();it != ll.end();it++)
+        cout << *it << endl;
+    return 0;
+}
