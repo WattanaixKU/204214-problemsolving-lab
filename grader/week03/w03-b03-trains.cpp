e3e3c6cf621751ba2bce1bdf68ce274f2cd5cd68
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-02-05 00:25:40 +0700

    PPxxPxxx

---
+++ grader/week03/w03-b03-trains.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,inp1,inp2;
+    char command;
+    cin >> n;
+    vector<int> station[100000];
+    bool in_use[100000];
+    for(int i=0;i<100000;i++)
+        in_use[i] = 0;
+    for(int e=0;e<n;e++)
+    {
+        cin >> command >> inp1 >> inp2;
+        if(command=='N')
+        {
+            in_use[inp2] = 1;
+            station[inp2].push_back(inp1);
+        }
+        else
+        {
+            in_use[inp1] = 0;
+            in_use[inp2] = 1;
+            for(int i=0;i<station[inp1].size();i++)
+                station[inp2].push_back(station[inp1][i]);
+            station[inp1].clear();
+        }
+    }
+    for(int i=0;i<100000;i++)
+        if(in_use[i])
+            for(int j=0;j<station[i].size();j++)
+                cout << station[i][j] << endl;
+    return 0;
+}
