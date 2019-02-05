e1db698c1a70f2b91f2bd57c94d1d0740608bd72
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-02-05 22:07:42 +0700

    map&vector = P-PxP-T-

--- grader/week03/w03-b03-trains.cpp
+++ grader/week03/w03-b03-trains.cpp
@@ -1,12 +1,17 @@
 #include <vector>
+#include <map>
 #include <iostream>
 using namespace std;
+struct cargo{
+    int num;
+    cargo* next;
+};
 int main()
 {
     int n,inp1,inp2;
     char command;
     cin >> n;
-    vector<int> station[100000];
+    map<int,vector<int> > station;
     bool in_use[100000];
     for(int i=0;i<100000;i++)
         in_use[i] = 0;
@@ -15,13 +20,28 @@ int main()
         cin >> command >> inp1 >> inp2;
         if(command=='N')
         {
-            in_use[inp2] = 1;
-            station[inp2].push_back(inp1);
+            if(in_use[inp2]==0)
+            {
+                in_use[inp2] = 1;
+                vector<int> tmpv;
+                tmpv.push_back(inp1);
+                //station[inp2].push_back(inp1);
+                station.insert(pair<int,vector<int> > (inp2, tmpv));
+            }
+            else
+                station[inp2].push_back(inp1);
         }
         else
         {
-            in_use[inp1] = 0;
-            in_use[inp2] = 1;
+            if(in_use[inp2]==0 && station[inp1].size()!=0)
+            {
+                in_use[inp2] = 1;
+                vector<int> tmpv;
+                tmpv.push_back(station[inp1][0]);
+                station[inp1].erase(station[inp1].begin());
+                station.insert(pair<int,vector<int> > (inp2, tmpv));
+            }
+            //in_use[inp1] = 0;
             int tmp = station[inp1].size();
             for(int i=0;i<tmp;i++)
             {
