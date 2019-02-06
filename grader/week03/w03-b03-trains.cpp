42ad33236fae1f0c6b7bc5e175238e6f26f4dfaa
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-02-06 10:13:52 +0700

    tailed still T

--- grader/week03/w03-b03-trains.cpp
+++ grader/week03/w03-b03-trains.cpp
@@ -1,5 +1,3 @@
-//#include <vector>
-//#include <map>
 #include <iostream>
 using namespace std;
 struct cargo{
@@ -11,45 +9,35 @@ int main()
     int n,inp1,inp2;
     char command;
     cin >> n;
-    cargo* station[100001];
-    cargo* tail[100001];
-    //bool in_use[100000];
-    for(int i=0;i<100001;i++)
+    cargo* station[100000];
+    cargo* tail[100000];
+    for(int i=0;i<100000;i++)
     {
         station[i] = NULL;
         tail[i] = NULL;
-        //in_use[i] = 0;
     }
     for(int e=0;e<n;e++)
     {
         cin >> command >> inp1 >> inp2;
+        inp2--;
         if(command=='N')
         {
             cargo* newOne = new cargo;
             newOne->num = inp1;
             newOne->next = NULL;
             if(tail[inp2]!=NULL)
-            {
-                cargo* cur = tail[inp2];
-                /*while(cur->next!=NULL)
-                    cur = cur->next;*/
-                cur->next = newOne;
-            }
+                tail[inp2]->next = newOne;
             else
                 station[inp2] = newOne;
             tail[inp2] = newOne;
         }
         else
         {
+            inp1--;
             if(station[inp1]==NULL)
                 continue;
             if(tail[inp2]!=NULL)
-            {
-                cargo* cur = tail[inp2];
-                /*while(cur->next!=NULL)
-                    cur = cur->next;*/
-                cur->next = station[inp1];
-            }
+                tail[inp2]->next = station[inp1];
             else
                 station[inp2] = station[inp1];
             station[inp1] = NULL;
@@ -57,7 +45,7 @@ int main()
             tail[inp1] = NULL;
         }
     }
-    for(int i=0;i<100001;i++)
+    for(int i=0;i<100000;i++)
         while(station[i]!=NULL)
         {
             cout << station[i]->num << endl;
