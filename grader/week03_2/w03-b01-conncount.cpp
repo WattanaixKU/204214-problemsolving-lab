43d1abd4f2be80c4890b92ec5451e0e8be9a244e
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-03-21 16:24:40 +0700

    PPPPPPP

--- grader/week03_2/w03-b01-conncount.cpp
+++ grader/week03_2/w03-b01-conncount.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stack>
+#include <vector>
 using namespace std;
 int main()
 {
@@ -7,37 +8,55 @@ int main()
     int n,m,start,d,count = 0;
     pair<int,bool> c;
     cin >> n >> m;
-    bool tbl[n][n];
+    //bool tbl[n][n];
+    vector<vector<int> > tbl;
+    //cout << "pass1" << endl;
     bool visited[n];
+    vector<int> tv;
     for(int i=0;i<n;i++)
-        for(int j=0;j<n;j++)
-            tbl[i][j] = 0;
+    {
+        tbl.push_back(tv);
+    }
     for(int i=0;i<m;i++)
     {
         cin >> start >> d;
+        //cout << start << d << endl;
         start--;
         d--;
-        tbl[start][d] = 1;
-        tbl[d][start] = 1;
+        //tbl[start][d] = true;
+        //tbl[d][start] = true;
+        tbl[start].push_back(d);
+        tbl[d].push_back(start);
     }
+    /*
     for(int i=0;i<n;i++)
     {
-        visited[i] = 0;
-        s.push(make_pair(i,1));
+        for(int j=0;j<n;j++)
+            cout << tbl[i][j];
+        cout << endl;
+    }*/
+
+    for(int i=0;i<n;i++)
+    {
+        visited[i] = false;
+        s.push(make_pair(i,true));
     }
     while(!s.empty())
     {
+        //cout << "size of stack" << s.size() << endl;
         c = s.top();
+        //cout << "\nstart " << c.first << " " << c.second;
         s.pop();
         if(visited[c.first])
             continue;
-        visited[c.first] = 1;
-        if(c.second == 1)
+        //cout << "go on";
+        visited[c.first] = true;
+        if(c.second)
             count++;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<tbl[c.first].size();i++)
         {
-            if(tbl[c.first][i] == 1)
-                s.push(make_pair(i,0));
+            if(!visited[tbl[c.first][i]])
+                s.push(make_pair(tbl[c.first][i],false));
         }
     }
     cout << count << endl;
