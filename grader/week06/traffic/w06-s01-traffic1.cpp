b3b4ec755a43bcbe0cbef0bf85950c9ac873008f
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-03-26 23:03:14 +0700

    k = 1 passed

---
+++ grader/week06/traffic/w06-s01-traffic1.cpp
@@ -0,0 +1,83 @@
+#include "traffic.h"
+#include <iostream>
+using namespace std;
+int mid,tmp;
+bool in;
+pair<pair<int,int>,pair<int,int> > ans1;
+pair<pair<int,int>,pair<int,int> > ans2;
+
+void check(int line, int start, int stop, int n, bool ver)
+{
+    if(stop-start==1)
+    {
+        if(ver)
+        {
+            //cout << line << " " << start << " " << line << " " << stop << endl;
+            if(ans1.first.first)
+                ans2 = make_pair(make_pair(line,start),make_pair(line,stop));
+            else
+                ans1 = make_pair(make_pair(line,start),make_pair(line,stop));
+        }
+        else
+        {
+            //cout << start << " " << line << " " << stop << " " << line << endl;
+            if(ans1.first.first)
+                ans2 = make_pair(make_pair(start,line),make_pair(stop,line));
+            else
+                ans1 = make_pair(make_pair(start,line),make_pair(stop,line));
+        }
+    }
+    else
+    {
+        mid = (stop+start)/2;
+        if(ver)
+            tmp = traffic_query(line,start,line,mid);
+        else
+            tmp = traffic_query(start,line,mid,line);
+        //cout << start << " " << line << " " << stop << " " << line << " - " << tmp << endl;
+        in = false;
+        if((tmp-(mid-start)) >= 1)
+        {
+            in = true;
+            check(line,start,mid,tmp,ver);
+        }
+        else
+            check(line,mid,stop,n-tmp-1,ver);
+        if(((n-tmp)-(stop-mid)) >= 1 && in)
+            check(line,mid,stop,n-tmp-1,ver);
+    }
+}
+int main()
+{
+    int n,k,t;
+    ans1 = make_pair(make_pair(0,0),make_pair(0,0));
+    ans2 = make_pair(make_pair(0,0),make_pair(0,0));
+    traffic_init(&n, &k);
+    for(int i=1; i<=n && k; i++)
+    {
+        t = traffic_query(i,1,i,n);
+        if(t!=(n-1))
+        {
+            check(i,1,n,t,true);
+            k -= t-(n-1);
+        }
+    }
+    for(int i=1; i<=n && k; i++)
+    {
+        t = traffic_query(1,i,n,i);
+        if(t!=(n-1))
+        {
+            check(i,1,n,t,false);
+            k -= t-(n-1);
+        }
+    }
+    traffic_report( ans1.first.first,
+                    ans1.first.second,
+                    ans1.second.first,
+                    ans1.second.second, 
+                    ans2.first.first,
+                    ans2.first.second,
+                    ans2.second.first,
+                    ans2.second.second);
+    return 0;
+}
