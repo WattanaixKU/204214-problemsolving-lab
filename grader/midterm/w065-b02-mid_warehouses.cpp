a66d673c7e9b1bc5c1993fceabe6d6f9bbabf4f6
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-04-01 03:03:00 +0700

    50/100

--- grader/midterm/w065-b02-mid_warehouses.cpp
+++ grader/midterm/w065-b02-mid_warehouses.cpp
@@ -2,57 +2,36 @@
 #include <vector>
 using namespace std;
 vector<vector<int> > vvi;
+vector<bool> visited;
+bool mfind(int s,int d)
+{
+    if(s==d)
+        return 1;
+    if(visited[s])
+        return 0;
+    visited[s] = 1;
+    bool ans = 0;
+    for(int i = 0;!ans && i < vvi[s].size();i++)
+        ans = ans || mfind(vvi[s][i],d);
+    return ans;
+}
 int main()
 {
     int n,m,k,s,f;
-    vector<int>::iterator itf,its;
     cin >> n >> m >> k;
     vector<int> t;
-    t.push_back(0);
     for(int i=0;i<n;i++)
     {
-        t[0] = i;
         vvi.push_back(t);
+        visited.push_back(0);
     }
     for(int i=0;i<m;i++)
     {
         cin >> s >> f;
         s--;
         f--;
-        t.clear();
-        itf = vvi[f].begin();
-        its = vvi[s].begin();
-        while(itf!=vvi[f].end() || its!=vvi[s].end())
-        {
-            if(itf == vvi[f].end())
-            {
-                if(t.back()!=*its)
-                    t.push_back(*its);
-                its++;
-                continue;
-            }
-            if(its == vvi[s].end())
-            {
-                if(t.back()!=*itf)
-                    t.push_back(*itf);
-                itf++;
-                continue;
-            }
-            if(*its <= *itf)
-            {
-                if(t.back()!=*its)
-                    t.push_back(*its);
-                its++;
-            }
-            else
-            {
-                if(t.back()!=*itf)
-                    t.push_back(*itf);
-                itf++;
-            }
-        }
-        vvi[s] = t;
-        vvi[f] = t;
+        vvi[s].push_back(f);
+        vvi[f].push_back(s);
     }
     bool find;
     for(int i=0;i<k;i++)
@@ -60,19 +39,12 @@ int main()
         cin >> s >> f;
         s--;
         f--;
-        find = 0;
-        for(int j=0;j<vvi[s].size() && !find;j++)
-        {
-            cout << vvi[s][j] << " ";
-            if(vvi[s][j] == f)
-            {
-                cout << 1 << endl;
-                find = 1;
-            }
-        }
-        if(!find)
+        for(int j=0;j<n;j++)
+            visited[j] = 0;
+        if(mfind(s,f))
+            cout << 1 << endl;
+        else
             cout << 0 << endl;
     }
-    
     return 0;
 }
