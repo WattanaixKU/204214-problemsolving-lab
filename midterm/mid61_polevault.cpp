306226f7874eb0e5d703299053bad018c6da2bf3
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-04-01 15:00:17 +0700

    100

--- midterm/mid61_polevault.cpp
+++ midterm/mid61_polevault.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
+vector<int> ans;
+bool bst(int s, int f,int x)
+{
+    if(s-f>=1)
+        return 0;
+    int mid = (f+s)/2;
+//    cout << s << " " << f << ans[mid] << endl;
+    if(ans[mid] == x)
+        return 1;
+    if(ans[mid] < x)
+        return bst(mid+1,f,x);
+    return bst(s,mid-1,x);
+}
 int main()
 {
-    int n,k,tmp = 0,mx = -1;
+    int n,k,tmp = 0;
     cin >> n >> k;
     int a[n],q[k];
     for(int i=0;i<n;i++)
@@ -10,29 +25,22 @@ int main()
     for(int i=0;i<k;i++)
     {
         cin >> q[i];
-        mx = max(mx,q[i]);
     }
-    mx++;
-    int ans[n][n];
     for(int i=0;i<n;i++)
     {
         tmp = 0;
         for(int j=i;j<n;j++)
         {
             tmp += a[j];
-            ans[i][j] = tmp;
+            ans.push_back(tmp);
         }
     }
+    sort( ans.begin(), ans.end() );
+    ans.erase( unique( ans.begin(), ans.end() ), ans.end() );
+    int ns = ans.size();
     for(int ii=0;ii<k;ii++)
     {
-        bool find = 0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i;j<n;j++)
-                if(ans[i][j] == q[ii])
-                    find = 1;
-        }
-        if(find)
+        if(bst(0,ns,q[ii]))
             cout << 'Y';
         else
             cout << 'N';
