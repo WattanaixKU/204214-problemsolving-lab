8566b0292d741e419277dedabe7824a47dbf72e2
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-04-01 16:33:12 +0700

    80

--- midterm/mid61_dig.cpp
+++ midterm/mid61_dig.cpp
@@ -1,29 +1,10 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
 using namespace std;
+typedef pair<pair<int,int>,pair<int,int> > pii;
 int n,m,x,y;
 char tbl[51][51];
-int go(int cx, int cy, int ox, int oy, bool dig)
-{
-    if(cx == x && cy == y)
-        return 0;
-    if(tbl[cx][cy] == '#')
-        return 10000;
-//    cout << cx << " " << cy << tbl[cx][cx]<< endl;
-    if(tbl[cx][cy] == '*')
-    {
-        if(!dig)
-            return 10000;
-        dig = 0;
-        if(ox == cx)
-            return 1+min(go(cx,cy-1,cx,cy,dig), go(cx,cy+1,cx,cy,dig));
-        else
-            return 1+min(go(cx-1,cy,cx,cy,dig), go(cx+1,cy,cx,cy,dig));
-    }
-    tbl[cx][cy] = '#';
-    return 1+min(
-            min(go(cx-1,cy,cx,cy,dig), go(cx+1,cy,cx,cy,dig)), 
-            min(go(cx,cy-1,cx,cy,dig), go(cx,cy+1,cx,cy,dig)) );
-}
 int main()
 {
     cin >> n >> m;
@@ -36,24 +17,35 @@ int main()
     for(int i=0;i<n;i++)
         for(int j=0;j<m;j++)
             cin >> tbl[i][j];
- /*   for(int i=0;i<n;i++)
+
+    priority_queue<pii, vector<pii>, greater<pii> > pq;
+    pq.push(make_pair(make_pair(0,0),make_pair(x1,y1)));
+    bool find = 0;
+    while(!pq.empty())
     {
-        for(int j=0;j<m;j++)
-            cout << tbl[i][j];
-        cout <<endl;
+        pii c = pq.top();
+        pq.pop();
+        if(c.second.first == x && c.second.second == y)
+        {
+            cout << c.first.first << endl;
+            find = 1;
+            break;
+        }
+        if(tbl[c.second.first][c.second.second] == '#')
+            continue;
+        if(tbl[c.second.first][c.second.second] == '*')
+        {
+            if(c.first.second)
+                continue;
+            c.first.second = 1;
+        }
+        tbl[c.second.first][c.second.second] = '#';
+        pq.push(make_pair(make_pair(1+c.first.first,c.first.second),make_pair(c.second.first+1,c.second.second)));
+        pq.push(make_pair(make_pair(1+c.first.first,c.first.second),make_pair(c.second.first-1,c.second.second)));
+        pq.push(make_pair(make_pair(1+c.first.first,c.first.second),make_pair(c.second.first,c.second.second-1)));
+        pq.push(make_pair(make_pair(1+c.first.first,c.first.second),make_pair(c.second.first,c.second.second+1)));
     }
-    */
-    int ans = go(x1,y1,x1,y1,true);
-    if(ans < 10000)
-        cout << ans << endl;
-    else
+    if(!find)
         cout << -1 << endl;
-/*    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<m;j++)
-            cout << tbl[i][j];
-        cout <<endl;
-    }
-    */
     return 0;
 }
